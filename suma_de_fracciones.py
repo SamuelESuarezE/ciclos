7a95cec2633c47d4b887254d0b170e531c4655a2
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-01-20 12:24:49 -0500

    Ejercicio suma de fracciones solucionado

--- suma_de_fracciones.py
+++ suma_de_fracciones.py
@@ -1,18 +1,12 @@
 # Desarrolle un programa que permita trabajar con las potencias fraccionales de dos, es decir:
-denominadores = []
-acumulador_list = []
-acumulador = 0
-decimal = 100000
+potencia=1
+division=1
+acumulador=0
+print("Potencia | Fraccion |   Suma")
+print("         |          |        ")
 
-
-for x in range(10):
-    print(x) 
-
-for num in range(100):
-    denominador= 2**num
-    decimal = 1/denominador
-    denominadores.append(denominador)
-    acumulador+=decimal
-    acumulador_list.append(acumulador)
-    if decimal<=0.000001: break
-    
+while division>0.000001:
+    division=1/2**potencia
+    acumulador+=division
+    print(f"{str(potencia).center(8)} | {str(round(division, 6)).center(8)} | {str(round(acumulador, 6)).center(8)}")
+    potencia+=1
